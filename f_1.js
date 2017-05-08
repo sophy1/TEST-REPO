0fc9ff4b3a9d2cc41f9bf39b00f72ff66dfba14f
Kim Kangho <cimfalab@gmail.com>
2017-05-08 14:25:46 +0900

    Update f_1.js

--- f_1.js
+++ f_1.js
@@ -1,1 +1,14 @@
-console.log(1);
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App" style={{color: red; fontSize: 13px;}}>
+        <p className="App-intro">
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
