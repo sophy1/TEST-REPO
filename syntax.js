019927782dd70fdd5e8912b2795224224745a2a8
Kim Kangho <cimfalab@gmail.com>
2017-05-18 13:38:23 +0900

    Update syntax.js

--- syntax.js
+++ syntax.js
@@ -4,17 +4,3 @@ var obj = {
   size: 105,
   name: 'noname'
 }
-
-function loadingClose(a) {
-}
-
-function foo(e) {
-  var active, close;
-}
-
-function bar(e) {
-  var ddd, b3;
-}
-
-function loadingClose(a, b) {
-}
