54eb40905f73eefbfad420efd932b8303f786a76
Kim Kangho <cimfalab@gmail.com>
2017-09-06 12:03:57 +0900

    Update test-file.js

--- test-file.js
+++ test-file.js
@@ -1,4 +1,19 @@
-'use strict';
+import React from 'react';
 
-function foo(options) {
+class Foo extends React.Component {
+    render() {
+        return (
+            <div>
+                {this.props.items.length && `(${this.props.items.join(', ')})`} {/* BAD_LENGTH_CHECK alarm */}
+            </div>
+        );
+    }
+
+    coerce() {
+      var cssString = "";
+      var backPosition;
+      if (animatedBackground.length === 3) {
+          cssString += "backgroundPosition: " + backPosition + "px; "; // BAD_TYPE_COERCION alarm: Expression 'backPosition' has an undefined value and type-coerced to string type.
+      }
+    }
 }
