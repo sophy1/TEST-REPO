fa2dff0c33b4159df31587463fc2ad079ba17207
Kim Kangho <cimfalab@gmail.com>
2018-02-22 10:49:29 +0900

    Update f 2.js

--- f 2.js
+++ f 2.js
@@ -1,1 +1,11 @@
+var moduleInstance = new require('module').Module(); // CALL_REQUIRE_AS_CONSTRUCTOR alarm because 'new' operator is not applied for the constructor 'Module()'.
 
+    let obj = {
+        set name(n) {
+            this._name = n;
+        },
+        get name() {
+            return this._name;
+        }
+    }
+    obj.name = "John";
