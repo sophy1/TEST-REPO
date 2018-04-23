f1114dafd72083a7f58074eb1ac71ec98a2f5cf0
Kim Kangho <cimfalab@gmail.com>
2018-04-23 12:59:49 +0900

    Update react.js

--- react.js
+++ react.js
@@ -1,7 +1,17 @@
-import React from 'react';
+    import React from 'react';
 
-class Hello extends React.Component {
-    render() {
-        <div>Hello</div>; // BAD_REACT_API_RETURN_VALUE alarm because 'render()' does not return this React element.
+    class Hello extends React.Component {
+        constructor(props) {
+            super(props);
+            this.state = { msg: 'hi' };
+        }
+        static getDerivedStateFromProps(nextProps, prevState) {
+            if (nextProps.msg && nextProps.msg !== this.state.msg) { // REACT_STATIC_LIFECYCLE_INVALID_THIS alarm because 'this' is 'null' in static method so 'TypeError' occurs at this point when accessing 'this.state'.
+                return { msg: nextProps.msg };
+            }
+            return null;
+        }
+        render() {
+            return <div>{this.state.msg}</div>;
+        }
     }
-}
