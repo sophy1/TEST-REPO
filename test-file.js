efe6c8c6a847a2c69dac7f331a498f4057d6373d
Kim Kangho <cimfalab@gmail.com>
2018-07-05 12:30:36 +0900

    Update test-file.js

--- test-file.js
+++ test-file.js
@@ -1,18 +1,22 @@
-import Vue from 'vue';
+    import Vue from 'vue';
 
-// Example 1
-new Vue({
-  el: '#app',
-  data: {
-    $options: [ // VUE_RESERVED_PROPERTY_IN_OPTION alarm because the '$options' property is predefined in a Vue instance.
-      { id: 'opt1', name: 'my option 1' },
-      { id: 'opt2', name: 'my option 2' }
-    ]
-  },
-  methods: {
-    $set() { // VUE_RESERVED_PROPERTY_IN_OPTION alarm because the '$set' method is predefined in a Vue instance.
-      // do something
-    }
-  },
-  template: '<div @click="$set"><div v-for="opt in $options" :key="opt.id">{{ opt.name }}</div></div>'
-});
+    new Vue({
+      el: '#app',
+      template: '<div>{{ prevYear}} {{ nextYear }}</div>',
+      data: {
+        currentYear: new Date().getFullYear()
+      },
+      computed: {
+        prevYear() { // VUE_MISSING_RETURN_VALUE_IN_COMPUTED
+          this.currentYear - 1;
+        },
+        nextYear: {
+          get() { // VUE_MISSING_RETURN_VALUE_IN_COMPUTED
+            this.currentYear + 1;
+          },
+          set(nextYear) {
+            this.currentYear = nextYear - 1;
+          }
+        }
+      }
+    });
