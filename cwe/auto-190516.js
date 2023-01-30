4327e024dc48be19200e05175ae400c82eccf22d
sophy1 <jihye.sophy.won@gmail.com>
2023-01-30 17:37:42 +0900

    Update auto-190516.js

--- cwe/auto-190516.js
+++ cwe/auto-190516.js
@@ -3,13 +3,6 @@ function CWE_398() { // IDENTICAL_BRANCHES
         y = x;
     }
 }
-function CWE_476() { // NULL_POINTER
-    var obj= {};
-    var y = obj.x;  // UNINITIALIZED_LOCAL_VAR
-    console.log(y);
-}
-
-
 // This will show examples related with the Common Weakness Enumeration (CWE).
 function CWE_129(x) { // ARRAY_INDEX_NEGATIVE
     var arr = [1, 2, 3];
